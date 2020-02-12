46168a126e7455b0f158201cc369b0c5d60112ff
carlozdaniel <a20160383@gmail.com>
2020-02-12 11:26:46 -0600

    delate and finish the crud

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,10 @@ class ArticlesController < ApplicationController
    render json: @article
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
+  end 
+
 end
