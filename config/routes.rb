daa90c974458bcd81880377a95f6c07bb34af934
carlozdaniel <a20160383@gmail.com>
2020-02-12 11:05:16 -0600

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   get 'home/index'
   get "articles/new",to: "articles#new"
+  get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+
+  patch "/articles/:id", to: "articles#update", as: :article
   post "articles", to:"articles#create"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get "/Bienvenida", to:"home#index"
 end
+
+
+
